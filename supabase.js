276e256779803b0a1ec9c905b561ed885b6ec522
Raphael Da Silva Costa <rapcosta@outlook.com>
2026-02-16 14:26:31 -0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,13 +1,11 @@
 const SUPABASE_URL = "https://dopbnyuhvgsjapgttist.supabase.co"
-const SUPABASE_KEY = "SUA_CHAVE_PUBLICA_AQUI"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvcGJueXVodmdzamFwZ3R0aXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExOTk5NTAsImV4cCI6MjA4Njc3NTk1MH0.Es5uUU4vhFtADeB9PXapqAMJqI8N18iOJw6-12q1Pbw"
 
-/* cria cliente */
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 )
 
-/* deixa global */
 window.supabaseClient = supabaseClient
 
 console.log("🔥 Supabase conectado")
