0fb5042ff737c0611cbc9898dd28d8d09a8d2df9
Raphael Da Silva Costa <rapcosta@outlook.com>
2026-02-18 19:04:25 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const supabaseUrl = "https://dopbnyuhvgsjapgttist.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvcGJueXVodmdzamFwZ3R0aXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExOTk5NTAsImV4cCI6MjA4Njc3NTk1MH0.Es5uUU4vhFtADeB9PXapqAMJqI8N18iOJw6-12q1Pbw"
-
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
